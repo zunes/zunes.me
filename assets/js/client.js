b91920d405764e6b2e320a0b78b0913cc697888d
404invalid-user <user@invaliduser.uk.to>
2021-02-15 20:42:32 +0000

    new website fix the font though

---
+++ assets/js/client.js
@@ -0,0 +1,8 @@
+//for mobile navbar
+function openNav() {
+    document.getElementById("mobilenavbar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mobilenavbar").style.width = "0";
+}
